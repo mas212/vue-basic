b7d332b1849beaf071ae860db10a14800fb48940
mas212 <mas32957@gmail.com>
2020-03-21 20:26:30 +0700

    method property

---
+++ js/method-property.js
@@ -0,0 +1,19 @@
+var app = new Vue({
+	el:'#app',
+	data:{
+		message: 'hallo directive text',
+		int1 : 1,
+		int2 : 2,
+		result : null,
+	},
+	computed:{
+		sum : function(){
+			return this.int1 + this.int2;
+		}
+	},
+	methods:{
+		sumProcess: function(int3){
+			return this.result = this.int1 + this.int2 + int3;
+		}
+	}
+})
